8952d7cfe51b5b67ca54148525cc5b8a6808472a
srichatala <sri2vas@gmail.com>
2015-10-18 00:48:03 -0400

    player controller added and player can move left and right as well player can jump from one paltform to another plarform

---
+++ Platformer/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpHeight;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool grounded;
+
+    private bool doubleJump;
+
+    private Animator animator;
+	// Use this for initialization
+	void Start () {
+       animator =  GetComponent<Animator>();
+	}
+	
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (grounded)
+            doubleJump = false;
+        animator.SetBool("Gounded", grounded);
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpHeight);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && !doubleJump && !grounded)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpHeight);
+            doubleJump = true;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, 0);
+        }
+        animator.SetFloat("Speed", Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
+
+        if (GetComponent<Rigidbody2D>().velocity.x > 0)
+            transform.localScale = new Vector3(2f, 2f, 2f);
+        else if (GetComponent<Rigidbody2D>().velocity.x < 0)
+            transform.localScale = new Vector3(-2f, 2f, 2f);
+    }
+}
